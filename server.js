f282137dc9c7563f63bf9946014f70bc8a39d5b3
Gustavo <gustavomaurocamacho@hotmail.com>
2020-03-05 15:11:09 -0300

    deploy ready

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(`${__dirname}/public`)));
+app.use(express.static(path.join(`${__dirname}/build`)));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/public/index.html`));
+    res.sendFile(path.join(`${__dirname}/build/index.html`));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3333);
